7d80068dd17a7a4906f022736eca185b1d2742a2
Alucard2419 <osmel2419@gmail.com>
2021-11-19 23:56:59 -0500

    hoy

--- frontend/src/components/Home/CarouselServicios.js
+++ frontend/src/components/Home/CarouselServicios.js
@@ -2,14 +2,47 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
-import { Grid } from '@material-ui/core';
+import { Button, Grid, Hidden } from '@material-ui/core';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { useHistory } from "react-router";
-import axios from 'axios';
+import { styled } from '@material-ui/styles';
+import imaAnuncio from 'assets/img/ya2.png'
+import imaAnuncio2 from 'assets/img/ya1.png'
+
+const AnuncioMobil = styled(Grid)(({ theme }) => ({
+  backgroundImage: `url(${imaAnuncio2})`,
+  backgroundSize: 'none',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  width: '100%',
+  height: '250px',
+  marginRight: '20px',
+  marginLeft: '20px',
+  borderRadius: '20px'
+}))
+
+
+const Anuncio = styled(Grid)(({ theme }) => ({
+  backgroundImage: `url(${imaAnuncio2})`,
+  backgroundSize:  'none' ,
+  backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat',
+  width: '100%',
+  height: '250px',
+  marginRight: '20px',
+  marginLeft: '20px',
+  borderRadius: '20px'
+  
+}))
+
+
+
+
+
 
 const responsive = {
-  
+
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
@@ -31,16 +64,45 @@ const responsive = {
 
 
 const useStyles = makeStyles((theme) => ({
-  carousel:{
+  carousel: {
     width: "100%",
     marginTop: 10,
     marginBottom: 10,
     [theme.breakpoints.down('xs')]: {
       marginTop: 0,
-    marginBottom: 20,
+      marginBottom: 20,
     },
   },
-  carouselitems:{
+  logo: {
+   
+    marginLeft: 20,
+    marginBottom: 20,
+    height: 140,
+  [theme.breakpoints.down('lg')]: {
+      width: '85%',
+      height: 80,
+      
+    },
+  [theme.breakpoints.down('xs')]: {
+    height: 40,
+  },
+},
+anuncio:{
+ justifyContent: 'center',
+ alignContent: 'center',
+ alignItems: 'center',
+ marginBottom: '20px',
+ [theme.breakpoints.down('md')]: {
+  height: 150,
+  borderRadius: '5px'
+},
+ [theme.breakpoints.down('xs')]: {
+  height: 120,
+  borderRadius: '5px'
+},
+ 
+},
+  carouselitems: {
   },
   root: {
     marginBottom: '0px',
@@ -50,12 +112,12 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
   },
   grid: {
-   
+
     padding: 10,
     [theme.breakpoints.down('xs')]: {
-        marginBottom: 0,
-        padding: 5,
-      },
+      marginBottom: 0,
+      padding: 5,
+    },
   },
   image: {
     position: 'relative',
@@ -98,7 +160,7 @@ const useStyles = makeStyles((theme) => ({
     bottom: 0,
     backgroundSize: 'cover',
     backgroundPosition: 'center 40%',
-  
+
 
   },
   imageBackdrop: {
@@ -117,7 +179,7 @@ const useStyles = makeStyles((theme) => ({
     padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
   },
   imageMarked: {
-    
+
     borderRadius: '24px',
     height: 3,
     width: 18,
@@ -133,89 +195,136 @@ const useStyles = makeStyles((theme) => ({
       paddingLeft: 15,
       paddingRight: 15,
     },
+
   },
+  accede: {
+    color: 'white',
+    fontSize: '45px',
+    [theme.breakpoints.down('lg')]: {
+      fontSize: '30px',
+      
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '20px',
+    },
+  },
+  boton: {
+      fontSize: '30px',
+      border: 'solid 5px white',
+      color: 'white',
+      [theme.breakpoints.down('lg')]: {
+        fontSize: '20px',
+      },
+      [theme.breakpoints.down('xs')]: {
+        fontSize: '15px',
+        border: 'solid 2px white',
+      },
+  },
+  
+
 }));
 
 
-export default function CarouselServicios({servicios}) {
-  
+export default function CarouselServicios({ servicios }) {
+
   const classes = useStyles();
   const history = useHistory();
-  
 
   return (
     <div className={classes.root}>
-        
-            <Carousel 
-            ssr
-            responsive={responsive}
-            containerClass={classes.carousel}
-            itemClass={classes.carouselitems}>
-              
-            {servicios.map((image,key) => {
-           const a = image.image
-          return (
-            
-              <Grid key={key} container className={classes.grid}>
-               {image.type === 'principal' ? <ButtonBase
-          onClick={()=>{
-            history.push(`serviciosprincipales/${image._id}`);
-          }}     
-          focusRipple
-          className={classes.image}
-          focusVisibleClassName={classes.focusVisible}
-          style={{
-            width: "100%",
-          }}
-        >
-     
-          <span
-            className={classes.imageSrc}
-            style={{
-           
-              backgroundImage: `url(${a})`,
-            }}
-            
-          />
-          <span className={classes.imageBackdrop} />
-          <span className={classes.imageButton}>
-            <Typography
-              component="span"
-              variant="subtitle1"
-              color="inherit"
-              className={classes.imageTitle}
-            >
-              {image.titulo}
-              <span className={classes.imageMarked} />
-            </Typography>
-          </span>
-        </ButtonBase> : null} 
-              
-              
-              </Grid>
-        
-      )})}
-    </Carousel>
 
-    <Grid 
-            container  
-            direction="row"
-            justifyContent="center"
-            alignItems="center">
-    <Typography
-              className={classes.subtitle}
-              color="inherit"
-            >
-             CriptoADVISER es un proyecto Fintech del tipo 3 pensada para facilitarle la vida 
-             a los criptoactivos debido a la cantidad de servicios que ofrecemos. Nos especializamos 
-             en la rama de la capacitación, el Trading con criptomonedas y la industria de los servicios. 
-             Tiene como objetivo principal dar a conocer el uso de las criptomonedas y mostrar la funcionalidad 
-             de las mismas, que poco a poco comienzan a abrirse paso en el mundo financiero, convirtiéndose en 
-             la principal estrategia económica de algunos países. Contamos con un excelente y preparado equipo 
-             de profesionales capaces de guiarlos por el camino del éxito para que alcancen la verdadera libertad financiera.
-            </Typography>
-    </Grid>
-      
+      <Carousel
+        ssr
+        responsive={responsive}
+        containerClass={classes.carousel}
+        itemClass={classes.carouselitems}>
+        {servicios.map((image, key) => {
+          const a = image.image
+          return (
+
+            <Grid key={key} container className={classes.grid}>
+              {image.type === 'principal' ? <ButtonBase
+                onClick={() => {
+                  history.push(`serviciosprincipales/${image._id}`);
+                }}
+                focusRipple
+                className={classes.image}
+                focusVisibleClassName={classes.focusVisible}
+                style={{
+                  width: "100%",
+                }}
+              >
+
+                <span
+                  className={classes.imageSrc}
+                  style={{
+
+                    backgroundImage: `url(${a})`,
+                  }}
+
+                />
+                <span className={classes.imageBackdrop} />
+                <span className={classes.imageButton}>
+                  <Typography
+                    component="span"
+                    variant="subtitle1"
+                    color="inherit"
+                    className={classes.imageTitle}
+                  >
+                    {image.titulo}
+                    <span className={classes.imageMarked} />
+                  </Typography>
+                </span>
+              </ButtonBase> : null}
+
+
+            </Grid>
+
+          )
+        })}
+      </Carousel>
+
+        <Anuncio container className={classes.anuncio} spacing={2}>
+          <Hidden smDown>
+          <Grid item  md={3} lg={4} xl={4}>
+           <img className={classes.logo} alt="logo" src={require("assets/img/logoblanco1.png")}/>
+          </Grid>
+          </Hidden>
+          <Grid item xs={12} md={5} lg={4} xl={4}>
+              <Typography  variant='h3' className={classes.accede}>
+                Accede a nuestros servicios y conviértase en <b>Miembro Oficial</b>
+              </Typography>
+          </Grid>
+          <Grid item xs={12} md={4} lg={4} xl={4}>
+            <Grid container style= {{justifyContent: 'center', alignItems: 'center', alignContent: 'center'}}>
+            <Button variant='outlined' size='large' className={classes.boton}>Accede Ahora</Button>
+           </Grid>
+          </Grid>
+
+        </Anuncio>
+
+ 
+
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center">
+
+        <Typography
+          className={classes.subtitle}
+          color="inherit"
+        >
+          CriptoADVISER es un proyecto Fintech del tipo 3 pensada para facilitarle la vida
+          a los criptoactivos debido a la cantidad de servicios que ofrecemos. Nos especializamos
+          en la rama de la capacitación, el Trading con criptomonedas y la industria de los servicios.
+          Tiene como objetivo principal dar a conocer el uso de las criptomonedas y mostrar la funcionalidad
+          de las mismas, que poco a poco comienzan a abrirse paso en el mundo financiero, convirtiéndose en
+          la principal estrategia económica de algunos países. Contamos con un excelente y preparado equipo
+          de profesionales capaces de guiarlos por el camino del éxito para que alcancen la verdadera libertad financiera.
+        </Typography>
+      </Grid>
+
     </div>
   );
 }
--- frontend/src/views/Freelancers/FreelancersPreView.js
+++ frontend/src/views/Freelancers/FreelancersPreView.js
@@ -3,7 +3,7 @@ import LandingNavBar from 'components/Home/landingNavbar';
 import LandingFooter from 'components/Home/LandingFooter';
 import { makeStyles } from '@material-ui/core';
 import { whiteColor } from 'assets/jss/material-dashboard-react';
-import { Grid, Hidden} from '@material-ui/core';
+import { Grid, Hidden } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import TelegramIcon from '@material-ui/icons/Telegram';
 import FacebookIcon from '@material-ui/icons/Facebook';
@@ -17,131 +17,129 @@ import { Helmet } from 'react-helmet';
 import ButtonV from './ButtonView';
 
 const useStyles = makeStyles((theme) => ({
-  
-    carousel: {
-      
-      backgroundPosition: 'center',
-      backgroundSize:'cover',
-      height:'280px',
-      padding: 40,
-      marginBottom: 0,
-      [theme.breakpoints.down('xs')]: {
-        marginBottom: 10,
-      },
+
+  carousel: {
+
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    height: '280px',
+    padding: 40,
+    marginBottom: 0,
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: 10,
     },
-    text: {
-      margin: 0,
-      color: whiteColor,
-    },
-    text1: {
-        margin: 0,
-      },
-    grid: {
-        padding: 40,
-        [theme.breakpoints.down('sm')]: {
-            paddingLeft: 10,
-            paddingRight: 10,
-            paddingTop: 0,
-            paddingBottom: 0,
-          },
-    },
-    grid2: {
-      padding: 40,
-      paddingTop: 0,
-      [theme.breakpoints.down('sm')]: {
-          paddingLeft: 10,
-          paddingRight: 10,
-          paddingTop: 0,
-          paddingBottom: 10,
-        },
   },
-    footer:{
-        position: 'fixed',
-        bottom: 0,
+  text: {
+    margin: 0,
+    color: whiteColor,
+  },
+  text1: {
+    margin: 0,
+  },
+  grid: {
+    padding: 20,
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: 10,
+      paddingRight: 10,
+      paddingTop: 0,
+      paddingBottom: 0,
     },
-    
-  }))
+  },
+  grid2: {
+    padding: 40,
+    paddingTop: 0,
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: 10,
+      paddingRight: 10,
+      paddingTop: 0,
+      paddingBottom: 10,
+    },
+  },
+  footer: {
+    position: 'relative',
+    top: 0,
+    bottom: 0,
+    width: '100%'
+  },
 
-const FreelancersPreView = (props)=> {
-    const history = useHistory();
-    const classes = useStyles();
-    const [freelancer] = useFreelancer()
+}))
 
-    React.useEffect( () => {
-      if (freelancer === null) {
-        history.push("/freelancers")
-      }
-      window.scrollTo(0,0)
-    },[])
-    return (
-        
-          freelancer === null ? null :
-          <>
-          <Helmet>
-                <title>{freelancer.titulo}</title>
-                <meta name="description" content={freelancer.descripcion} />
-            </Helmet>
-          <LandingNavBar/>
-         <Grid style={{backgroundImage: `url(${freelancer.image})`,}} container alignItems="center" className={classes.carousel}>
-         <Grid item md={12} sm={12} xs={12}>
-         <Hidden smDown>
-         <h1 className={classes.text}>{freelancer.titulo}</h1>
-         <h3 className={classes.text}>{freelancer.descripcion}</h3> 
-         </Hidden>
-         <Hidden mdUp>
-         <h3 className={classes.text}>{freelancer.titulo}</h3>
-         </Hidden>
-         </Grid>
-         </Grid>
-         <Hidden smDown>
-         <Grid style={{minHeight:220}} container justifyContent="center">
-             <Grid className={classes.grid} item md={8} sm={12} xs={12}>
-                 <h3 className={classes.text1}>Nombre: {freelancer.titulo}</h3>
-                 <h3 className={classes.text1}>Descripcion: {freelancer.descripcion}</h3>
-             </Grid> 
-             <Grid className={classes.grid} item md={4} sm={12} xs={12}>
-                         <h3 style={{display: freelancer.contactinfo.numero === "" ? "none" : null,margin: 0}}>Número de telefono:</h3>
-                         <h3 style={{display: freelancer.contactinfo.numero === "" ? "none" : null,margin: 0}}> +53 {freelancer.contactinfo.numero}</h3>  
-                         <h3 style={{margin:0}}> Redes:
-                         <IconButton href={freelancer.contactinfo.telegram} style={{display: freelancer.contactinfo.telegram === "" ? "none" : null}}> <TelegramIcon style={{color:"black"}}></TelegramIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.whatsapp} style={{display: freelancer.contactinfo.whatsapp === "" ? "none" : null}}> <WhatsAppIcon style={{color:"black"}}></WhatsAppIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.facebook} style={{display: freelancer.contactinfo.facebook === "" ? "none" : null}}> <FacebookIcon style={{color:"black"}}></FacebookIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.instagram} style={{display: freelancer.contactinfo.instagram === "" ? "none" : null}}> <InstagramIcon style={{color:"black"}}></InstagramIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.twitter} style={{display: freelancer.contactinfo.twitter === "" ? "none" : null}}> <TwitterIcon style={{color:"black"}}></TwitterIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.ubicacion} style={{display: freelancer.contactinfo.ubicacion === "" ? "none" : null}}> <LocationOnIcon style={{color:"black"}}></LocationOnIcon> </IconButton>
-                         </h3> 
-                 
-             </Grid> 
-         </Grid>
-         </Hidden>
-         <Hidden mdUp>
-         <Grid container justifyContent="center">
-             <Grid className={classes.grid} item md={8} sm={12} xs={12}>
-                 <h4 className={classes.text1}>Nombre: {freelancer.titulo}</h4>
-                 <h4 className={classes.text1}>Descripcion: {freelancer.descripcion}</h4>
-             </Grid> 
-             <Grid className={classes.grid} item md={4} sm={12} xs={12}>
-                         <h4 style={{display: freelancer.contactinfo.numero === "" ? "none" : null,margin: 0}}>Número de telefono:</h4>
-                         <h4 style={{display: freelancer.contactinfo.numero === "" ? "none" : null,margin: 0}}> +53 {freelancer.contactinfo.numero}</h4>  
-                         <h4 style={{margin:0}}> Redes:
-                         <IconButton href={freelancer.contactinfo.telegram} style={{display: freelancer.contactinfo.telegram === "" ? "none" : null}}> <TelegramIcon style={{color:"black"}}></TelegramIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.whatsapp} style={{display: freelancer.contactinfo.whatsapp === "" ? "none" : null}}> <WhatsAppIcon style={{color:"black"}}></WhatsAppIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.facebook} style={{display: freelancer.contactinfo.facebook === "" ? "none" : null}}> <FacebookIcon style={{color:"black"}}></FacebookIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.instagram} style={{display: freelancer.contactinfo.instagram === "" ? "none" : null}}> <InstagramIcon style={{color:"black"}}></InstagramIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.twitter} style={{display: freelancer.contactinfo.twitter === "" ? "none" : null}}> <TwitterIcon style={{color:"black"}}></TwitterIcon> </IconButton>
-                         <IconButton href={freelancer.contactinfo.ubicacion} style={{display: freelancer.contactinfo.ubicacion === "" ? "none" : null}}> <LocationOnIcon style={{color:"black"}}></LocationOnIcon> </IconButton>
-                         </h4> 
-                 
-             </Grid> 
-         </Grid>
-         </Hidden>
-        <Grid className={classes.grid2}>
-        <ButtonV></ButtonV>
+const FreelancersPreView = (props) => {
+  const history = useHistory();
+  const classes = useStyles();
+  const [freelancer] = useFreelancer()
+
+  React.useEffect(() => {
+    if (freelancer === null) {
+      history.push("/freelancers")
+    }
+    window.scrollTo(0, 0)
+  }, [])
+  return (
+
+    freelancer === null ? null :
+      <>
+        <Helmet>
+          <title>{freelancer.titulo}</title>
+          <meta name="description" content={freelancer.descripcion} />
+        </Helmet>
+        <LandingNavBar />
+        <Grid style={{ backgroundImage: `url(${freelancer.image})`, }} container alignItems="center" className={classes.carousel}>
+          <Grid item md={12} sm={12} xs={12}>
+            <Hidden smDown>
+              <h1 className={classes.text}>{freelancer.titulo}</h1>
+            </Hidden>
+            <Hidden mdUp>
+              <h3 className={classes.text}>{freelancer.titulo}</h3>
+            </Hidden>
+          </Grid>
         </Grid>
-         <LandingFooter className={classes.footer}></LandingFooter>
-         </>
-        
-    )
+        <Hidden smDown>
+          <Grid style={{ minHeight: 220 }} container >
+            <Grid className={classes.grid} item md={12} sm={12} xs={12}>
+              <Grid container >
+                <IconButton href={freelancer.contactinfo.telegram} style={{ display: freelancer.contactinfo.telegram === "" ? "none" : null }}> <TelegramIcon style={{ color: "black" }}></TelegramIcon> </IconButton>
+                <IconButton href={freelancer.contactinfo.whatsapp} style={{ display: freelancer.contactinfo.whatsapp === "" ? "none" : null }}> <WhatsAppIcon style={{ color: "black" }}></WhatsAppIcon> </IconButton>
+                <IconButton href={freelancer.contactinfo.facebook} style={{ display: freelancer.contactinfo.facebook === "" ? "none" : null }}> <FacebookIcon style={{ color: "black" }}></FacebookIcon> </IconButton>
+                <IconButton href={freelancer.contactinfo.instagram} style={{ display: freelancer.contactinfo.instagram === "" ? "none" : null }}> <InstagramIcon style={{ color: "black" }}></InstagramIcon> </IconButton>
+                <IconButton href={freelancer.contactinfo.twitter} style={{ display: freelancer.contactinfo.twitter === "" ? "none" : null }}> <TwitterIcon style={{ color: "black" }}></TwitterIcon> </IconButton>
+                <IconButton href={freelancer.contactinfo.ubicacion} style={{ display: freelancer.contactinfo.ubicacion === "" ? "none" : null }}> <LocationOnIcon style={{ color: "black" }}></LocationOnIcon> </IconButton>
+              </Grid>
+            </Grid>
+            <Grid className={classes.grid} item md={12} sm={12} xs={12}>
+              <h3 className={classes.text1}>Nombre: {freelancer.titulo}</h3>
+              <h3 className={classes.text1}>Descripcion: {freelancer.descripcion}</h3>
+            </Grid>
+
+          </Grid>
+        </Hidden>
+        <Hidden mdUp>
+          <Grid container justifyContent="center">
+            <Grid className={classes.grid} item md={12} sm={12} xs={12}>
+
+              <IconButton href={freelancer.contactinfo.telegram} style={{ display: freelancer.contactinfo.telegram === "" ? "none" : null }}> <TelegramIcon style={{ color: "black" }}></TelegramIcon> </IconButton>
+              <IconButton href={freelancer.contactinfo.whatsapp} style={{ display: freelancer.contactinfo.whatsapp === "" ? "none" : null }}> <WhatsAppIcon style={{ color: "black" }}></WhatsAppIcon> </IconButton>
+              <IconButton href={freelancer.contactinfo.facebook} style={{ display: freelancer.contactinfo.facebook === "" ? "none" : null }}> <FacebookIcon style={{ color: "black" }}></FacebookIcon> </IconButton>
+              <IconButton href={freelancer.contactinfo.instagram} style={{ display: freelancer.contactinfo.instagram === "" ? "none" : null }}> <InstagramIcon style={{ color: "black" }}></InstagramIcon> </IconButton>
+              <IconButton href={freelancer.contactinfo.twitter} style={{ display: freelancer.contactinfo.twitter === "" ? "none" : null }}> <TwitterIcon style={{ color: "black" }}></TwitterIcon> </IconButton>
+              <IconButton href={freelancer.contactinfo.ubicacion} style={{ display: freelancer.contactinfo.ubicacion === "" ? "none" : null }}> <LocationOnIcon style={{ color: "black" }}></LocationOnIcon> </IconButton>
+
+
+            </Grid>
+            <Grid className={classes.grid} item md={12} sm={12} xs={12}>
+              <h4 className={classes.text1}>Nombre: {freelancer.titulo}</h4>
+              <h4 className={classes.text1}>Descripcion: {freelancer.descripcion}</h4>
+            </Grid>
+
+          </Grid>
+        </Hidden>
+        <Grid className={classes.grid2}>
+          <ButtonV></ButtonV>
+        </Grid>
+        <LandingFooter className={classes.footer}></LandingFooter>
+      </>
+
+  )
 }
 
 export default FreelancersPreView;
